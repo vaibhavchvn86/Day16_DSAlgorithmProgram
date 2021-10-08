340a2a587b7a58282b928da1e6562d0f9951139e
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-08 23:16:28 +0530

    [VaibhavChavan]Added:Prime numbers in Range(0-1000)

---
+++ UC7_PrimeN.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day16_AlgoAssignment
+{
+    class UC7
+    {
+        public static void Prime()
+        {
+            int i=0;
+            Console.WriteLine("**********Welcome to Prime umber Program*********\n\nPrime Number Between 0-1000 are->");
+                Console.Write("2 3 5 7 ");
+            for (i = 2; i <= 1000; i++)
+            {
+                { 
+                if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                { }
+                else
+                    Console.Write(i+" ");
+                }
+            }
+        }
+    }
+}
