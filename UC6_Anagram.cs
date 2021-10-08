1baafc4ca2abc26ed3846daa50555764fefc86da
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-10-08 23:14:29 +0530

    [VaibhavChavan]Added:Checking Two Strings are Anagram or not

---
+++ UC6_Anagram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day16_AlgoAssignment
+{
+    class UC6_Anagram
+    {
+        public static void Anagram()
+        {
+            Console.WriteLine(" Enter 1st word");
+            string w1 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(w1);
+            Console.WriteLine(" Enter 2nd word");
+            string w2 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(w2);
+
+                char[] a = w1.ToLower().ToCharArray();
+                char[] b = w2.ToLower().ToCharArray();
+                Array.Sort(a);
+                Array.Sort(b);
+            string x = new string (a);
+            string y = new string (b);
+
+                if(x==y)
+            {
+                Console.WriteLine(w1 + " " + w2 + " are Anagram");
+            }
+            else
+            {
+                Console.WriteLine(w1 + " " + w2 + " are not Anagram");
+            }
+        }
+    }
+}
